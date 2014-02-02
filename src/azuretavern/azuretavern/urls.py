cd16f759697d5df9f69198b2202b48e9d1c2b01f
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2014-02-01 23:34:36 -0600

    First pass at registration, login, and logout.

--- src/azuretavern/azuretavern/urls.py
+++ src/azuretavern/azuretavern/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
-from .views import HomeView
+from .views import (HomeView, LoginView, LogoutView, RegisterView,
+    RegistrationCompleteView)
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -7,6 +8,15 @@ from .views import HomeView
 
 urlpatterns = patterns('',
     url(r'^$', HomeView.as_view(), name='home'),
+
+    # Registration/login
+    url(r'^account/login$', LoginView.as_view(), name='login'),
+    url(r'^account/logout$', LogoutView.as_view(), name='logout'),
+    url(r'^account/register$', RegisterView.as_view(), name='register'),
+    url(r'^account/created$',
+        RegistrationCompleteView.as_view(),
+        name='registration_complete'
+    )
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- src/azuretavern/azuretavern/views.py
+++ src/azuretavern/azuretavern/views.py
@@ -1,6 +1,30 @@
 """Site-wide views for the Azure Tavern."""
 
-from django.views.generic import TemplateView
+from django.core import urlresolvers
+from django.contrib.auth import logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.views.generic import FormView, RedirectView, TemplateView
 
 class HomeView(TemplateView):
     template_name = 'index.html'
+
+class LoginView(FormView):
+    form_class = AuthenticationForm
+    template_name = 'accounts/login.html'
+    success_url = urlresolvers.reverse_lazy('home')
+
+class RegisterView(FormView):
+    form_class = UserCreationForm
+    template_name = 'accounts/register.html'
+    success_url = urlresolvers.reverse_lazy('registration_complete')
+
+class RegistrationCompleteView(TemplateView):
+    template_name = 'accounts/complete.html'
+
+class LogoutView(RedirectView):
+    pattern_name = 'home'
+
+    def get(self, request, *args, **kwargs):
+        """Logs the user out."""
+        logout(request)
+        return super(LogoutView, self).get(request, *args, **kwargs)
