fb60dc748ca7f5ce310f286ca977e1a871640044
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2014-02-02 00:03:12 -0600

    Ensuring that LoginView actually logs user in.

--- src/azuretavern/azuretavern/views.py
+++ src/azuretavern/azuretavern/views.py
@@ -1,7 +1,7 @@
 """Site-wide views for the Azure Tavern."""
 
 from django.core import urlresolvers
-from django.contrib.auth import logout
+from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.views.generic import FormView, RedirectView, TemplateView
 
@@ -12,6 +12,10 @@ class LoginView(FormView):
     form_class = AuthenticationForm
     template_name = 'accounts/login.html'
     success_url = urlresolvers.reverse_lazy('home')
+
+    def form_valid(self, form):
+        login(self.request, form.user_cache)
+        return super(LoginView, self).form_valid(form)
 
 class RegisterView(FormView):
     form_class = UserCreationForm
