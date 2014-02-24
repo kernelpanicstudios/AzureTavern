a96b1840dcc2e4e7daccacb17abfcf508a7c05fc
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2014-02-24 13:50:06 -0600

    Added Character model.

--- src/azuretavern/rptavern/models.py
+++ src/azuretavern/rptavern/models.py
@@ -1,3 +1,30 @@
-from django.db import models
+"""Models needed for the RPTavern."""
 
-# Create your models here.
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User
+
+class Character(models.Model):
+    MALE = 'm'
+    FEMALE = 'f'
+    GENDER_CHOICES = (
+        (MALE, _('Male')),
+        (FEMALE, _('Female')),
+    )
+
+    player = models.ForeignKey(User)
+    full_name = models.CharField(max_length=200)
+    short_name = models.CharField(max_length=60)
+    gender = models.CharField(max_length=3, choices=GENDER_CHOICES, blank=True)
+    description = models.CharField(max_length=500)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        get_latest_by = 'modified_date'
+        unique_together = ('player', 'full_name')
+        verbose_name = _('character')
+        verbose_name_plural = _('characters')
+
+    def __unicode__(self):
+        return self.full_name
