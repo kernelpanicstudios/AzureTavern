7c2c75fe25ccc967264036d2916cc001ce8e593c
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2014-03-01 18:34:53 -0600

    Refactored MyCharacterListView for reuse.

--- src/azuretavern/rptavern/views.py
+++ src/azuretavern/rptavern/views.py
@@ -1,21 +1,38 @@
 """rptavern views."""
 
+from django.core.exceptions import ImproperlyConfigured
+from django.shortcuts import get_object_or_404
 from django.views.generic import CreateView, ListView
 from braces.views import LoginRequiredMixin
 from .models import Character
 
-class MyCharacterListView(LoginRequiredMixin, ListView):
+class AbstractCharacterListView(LoginRequiredMixin, ListView):
     context_object_name = 'characters'
+
+    def get_player(self):
+        raise ImproperlyConfigured('get_player() must be configured')
 
     def get_queryset(self):
         return Character.objects.filter(
-            player=self.request.user,
+            player=self.get_player(),
         ).order_by('full_name')
 
     def get_context_data(self, **kwargs):
-        context = super(MyCharacterListView, self).get_context_data(**kwargs)
-        context['player'] = self.request.user
+        context = super(AbstractCharacterListView, self).get_context_data(**kwargs)
+        context['player'] = self.get_player()
         return context
+
+class MyCharacterListView(AbstractCharacterListView):
+    def get_player(self):
+        return self.request.user
+
+class CharacterListView(AbstractCharacterListView):
+    player_id = None
+
+    def get_player(self):
+        if self.player_id is None:
+            raise ImproperlyConfigured('player_id must be provided')
+        return get_object_or_404(User, pk=self.player_id)
 
 class CharacterCreateView(LoginRequiredMixin, CreateView):
     model = Character
