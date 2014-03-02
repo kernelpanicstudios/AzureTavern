7e23dcfa0460c031969268cd1895942de229fc53
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2014-03-01 22:58:32 -0600

    First attempt at a CharacterCreateForm.

---
+++ src/azuretavern/rptavern/forms.py
@@ -0,0 +1,22 @@
+"""rptavern forms."""
+
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+from braces.forms import UserKwargModelFormMixin
+from rptavern.models import Character
+
+class CharacterCreateForm(UserKwargModelFormMixin, forms.ModelForm):
+    class Meta:
+        model = Character
+        fields = ['player', 'short_name', 'full_name', 'gender', 'description']
+        widgets = {
+            'player': forms.HiddenInput,
+        }
+
+    def clean_player(self):
+        if self.cleaned_data['player'] != self.user:
+            raise forms.ValidationError(
+                _('You cannot create characters for other users'),
+                code='invalid_player',
+            )
+        return self.cleaned_data['player']
--- src/azuretavern/rptavern/views.py
+++ src/azuretavern/rptavern/views.py
@@ -3,7 +3,8 @@
 from django.core.exceptions import ImproperlyConfigured
 from django.shortcuts import get_object_or_404
 from django.views.generic import CreateView, ListView
-from braces.views import LoginRequiredMixin
+from braces.views import LoginRequiredMixin, UserFormKwargsMixin
+from .forms import CharacterCreateForm
 from .models import Character
 
 class AbstractCharacterListView(LoginRequiredMixin, ListView):
@@ -34,9 +35,9 @@ class CharacterListView(AbstractCharacterListView):
             raise ImproperlyConfigured('player_id must be provided')
         return get_object_or_404(User, pk=self.player_id)
 
-class CharacterCreateView(LoginRequiredMixin, CreateView):
+class CharacterCreateView(LoginRequiredMixin, UserFormKwargsMixin, CreateView):
     model = Character
-    # TODO: Create a form class and use it to choose important fields
+    form_class = CharacterCreateForm
 
     def get_initial(self):
         return {
