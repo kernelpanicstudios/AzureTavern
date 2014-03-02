304a898fed3386467d3a9193c37700b7f9fb2503
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2014-03-01 23:02:54 -0600

    RPTavern urlconf fix.
    Modified rptavern URLs to include end-of-string anchors.

--- src/azuretavern/rptavern/urls.py
+++ src/azuretavern/rptavern/urls.py
@@ -4,10 +4,10 @@ from django.conf.urls import include, patterns, url
 from .views import MyCharacterListView, CharacterCreateView
 
 urlpatterns = patterns('',
-    url('characters/me', MyCharacterListView.as_view(),
+    url(r'characters/me$', MyCharacterListView.as_view(),
         name='my-characters'
     ),
-    url('characters/new', CharacterCreateView.as_view(),
+    url(r'characters/new$', CharacterCreateView.as_view(),
         name='new-character'
     ),
 )
