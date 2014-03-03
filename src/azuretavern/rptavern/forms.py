47a7d68b8afa9124c7ea1f3f5c97d6eade9cedc6
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2014-03-02 18:21:45 -0600

    Cleaning up GameForm.

--- src/azuretavern/rptavern/forms.py
+++ src/azuretavern/rptavern/forms.py
@@ -24,3 +24,15 @@ class CharacterForm(UserKwargModelFormMixin, forms.ModelForm):
 class GameForm(UserKwargModelFormMixin, forms.ModelForm):
     class Meta:
         model = Game
+        fields = ['gm', 'title', 'subtitle', 'description']
+        widgets = {
+            'gm': forms.HiddenInput,
+        }
+
+    def clean_gm(self):
+        if self.cleaned_data['gm'] != self.user:
+            raise forms.ValidationError(
+                _('You cannot create games for other users to GM'),
+                code='invalid_gm',
+            )
+        return self.cleaned_data['gm']
